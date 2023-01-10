fa0dd66ab39d3feeddf746ea86db4a1130e1aac1
Vitor Hugo de Almeida Junior <vitorhugo.jr@hotmail.com>
2023-01-10 10:45:11 -0300

    Update vectorial geo angle

--- demo/demo1_geo_ang.m
+++ demo/demo1_geo_ang.m
@@ -1,9 +1,11 @@
+setup_spherical_reflection()
+
 Ha_dom = 100;
 e_dom = 0:1:89.9;
 Rs = get_earth_radius();
 [e_grid, Ha_grid] = ndgrid (e_dom, Ha_dom);
 
-[~, g, ~, ~, X_spec, Y_spec, X_trans, Y_trans] = get_reflection_spherical(e_grid, Ha_grid);
+[~, g, ~, ~, X_spec, Y_spec, X_trans, Y_trans] = get_reflection_spherical(e_grid, Ha_grid, [], [], [], [], 'quasigeo');
         
 pos_spec(1,:) = X_spec;
 pos_spec(2,:) = Y_spec; 
@@ -12,7 +14,7 @@ pos_trans(1,:) = X_trans;
 pos_trans(2,:) = Y_trans; 
 
 pos_ant (1,:) = zeros(size(e_dom));
-pos_ant (2,:) = Ha_dom;
+pos_ant (2,:) = Rs+Ha_dom;
 
 geo_ang_as_vec = get_geocentric_angle_vec (pos_ant', pos_spec');
 geo_ang_at_vec = get_geocentric_angle_vec (pos_ant', pos_trans');
--- geo-alhazen-aux/get_geocentric_angle_vec.m
+++ geo-alhazen-aux/get_geocentric_angle_vec.m
@@ -3,16 +3,16 @@ function geo_ang = get_geocentric_angle_vec (pos_ant, pos)
 % Returns geocentric angle of a target (vectorial formulation)
 %
 % OUTPUT:
-% - geo_ang: Target geocentric angle
+% - geo_ang: geocentric angle between antenna and target (vector, in degrees)
 % 
 % INPUT:
-% - pos_ant: Antenna position (vector, in meters)
-% - pos: Target position (vector, in meters)
+% - pos_ant: Antenna geocentric position (vector, in meters)
+% - pos: Target geocentric position (vector, in meters)
 % 
 % Note: target may be surface or transmitter.
 
 %% Geocentric Angle:
 temp = dot_all(pos, pos_ant)./(norm_all(pos).*norm_all(pos_ant));
-geo_ang = 2.*(acosd(temp)-90);
+geo_ang = (acosd(temp));
 
 end
