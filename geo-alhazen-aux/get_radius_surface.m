b60514db4843da2f026ce2798c8cb3de844fefe7
Felipe Geremia-Nievinski <fgnievinski@gmail.com>
2023-05-03 12:06:18 -0300

    Delete get_radius_surface.m

--- geo-alhazen-aux/get_radius_surface.m
+++
@@ -1,9 +0,0 @@
-function Rs = get_radius_surface (lat, hs, ell)
-
-    if (nargin < 1) || isempty(lat),  lat = 0;  end
-    if (nargin < 2) || isempty(hs),  hs = 0;  end
-    if (nargin < 3) || isempty(ell),  ell = get_ellipsoid();  end
-    Rc = get_radius_gaussian(lat, ell);
-    Rs = Rc + hs;
-   
-end
