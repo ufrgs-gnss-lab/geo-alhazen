1bd7e9baa583422e1bfe310d23e79b51be1c0fbc
Vitor Hugo de Almeida Junior <vitorhugo.jr@hotmail.com>
2024-09-22 20:44:14 -0300

    Update get_reflection_spherical_vuorinen.m

--- geo-alhazen-aux/get_reflection_spherical_vuorinen.m
+++ geo-alhazen-aux/get_reflection_spherical_vuorinen.m
@@ -1,4 +1,4 @@
-function [graz_ang, geo_ang_as, x_spec, y_spec, dx_trans, dy_trans] = get_reflection_spherical_vuorinen (e, Ha, Rs, choice_method)
+function [graz_ang, geo_ang_as, x_spec, y_spec, dx_trans, dy_trans, di, slant_dist] = get_reflection_spherical_vuorinen (e, Ha, Rs, choice_method)
 % GET_REFLECTION_SPHERICAL_VUORINEN Calculates reflection on spherical 
 % surface based on Vuorinen (2023) equations (internal document - filename cal20230511.tex)
 %
@@ -48,6 +48,9 @@ dx_trans = cosd(e);
 dy_trans = sind(e);
 dir_trans = [dx_trans, dy_trans];
 
+% Antenna's local position
+pos_ant_local = [0 Ha]; 
+
 %% Root choice process
 if strcmp(choice_method,'heuristic')
 
@@ -56,18 +59,15 @@ if strcmp(choice_method,'heuristic')
     ind = argmin(abs(phis2-theta));
     
 elseif strcmp(choice_method,'rigorous')
-    
-    pos_ant_local = [0 Ha]; 
+
     di0 = 3*(Ha);
 
     for i=1:numel (ws)
 
        w1 = ws(i);
        pos_spec1 = pos_spec_complex (w1, theta, Rs);
-       slant_dist = norm_all(pos_ant_local - pos_spec1);
-       [~, delay_direct_aux] = proj_pt_line (pos_spec1, pos_ant_local, dir_trans);
-       di1 = slant_dist - delay_direct_aux;
-
+       [di1] = interf_delay_infinite_distance (pos_spec1, pos_ant_local, dir_trans);
+       
        if di1<di0
            di0 = di1; 
            ind0 = i;
@@ -80,11 +80,15 @@ w = ws(ind);
 phi = phis(ind);
 %%
 % Geocentric angle at reflection point:
-geo_ang_as = deg2rad (theta-phi);
+geo_ang_as = rad2deg(theta-phi);
 
 % Reflection point in a quasigeocentric frame
 [~, x_spec, y_spec] = pos_spec_complex (w, theta, Rs);
+pos_spec = [x_spec y_spec];
 pos_spec_geo = [x_spec y_spec+Rs];
+
+% Interferometric delay
+[di, slant_dist] = interf_delay_infinite_distance (pos_spec, pos_ant_local, dir_trans);
 
 % Antenna geocentric position
 pos_ant_geo = [0 Ra];
@@ -96,9 +100,17 @@ end
 
 function [pos_spec, x_spec, y_spec] = pos_spec_complex (w, theta, Rs)
 
-pos_spec_complex = w./exp(-1i*(pi./2-theta)).*Rs-complex(0,Rs);
-x_spec = real(pos_spec_complex);
-y_spec = imag(pos_spec_complex);
-pos_spec = [x_spec, y_spec];
+    pos_spec_complex = w./exp(-1i*(pi./2-theta)).*Rs-complex(0,Rs);
+    x_spec = real(pos_spec_complex);
+    y_spec = imag(pos_spec_complex);
+    pos_spec = [x_spec, y_spec];
+
+end
+
+function [di,slant_dist] = interf_delay_infinite_distance (pos_spec, pos_ant_local, dir_trans)
+
+    slant_dist = norm_all(pos_ant_local - pos_spec);
+    [~, delay_direct_aux] = proj_pt_line (pos_spec, pos_ant_local, dir_trans);
+    di = slant_dist - delay_direct_aux;
 
 end
