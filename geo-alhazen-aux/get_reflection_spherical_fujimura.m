3ee5bcfe6ea4a42b2d393212c7d4ec565cba011c
Vitor Hugo de Almeida Junior <vitorhugo.jr@hotmail.com>
2025-09-19 08:38:53 -0300

    Update get_reflection_spherical_fujimura.m

--- geo-alhazen-aux/get_reflection_spherical_fujimura.m
+++ geo-alhazen-aux/get_reflection_spherical_fujimura.m
@@ -1,4 +1,4 @@
-function [graz_ang, geo_ang_as, x_spec, y_spec, x_trans, y_trans] = get_reflection_spherical_fujimura (e, Ha, Ht, Rs)
+function [graz_ang, geo_ang_as, x_spec, y_spec, x_trans, y_trans, u] = get_reflection_spherical_fujimura (e, Ha, Ht, Rs)
 
 % GET_REFLECTION_SPHERICAL_FUJIMURA Calculates reflection on spherical 
 % surface based on Fujimura et al. (2019) equations.
@@ -20,13 +20,9 @@ function [graz_ang, geo_ang_as, x_spec, y_spec, x_trans, y_trans] = get_reflecti
 % - geo_ang_as: geocentric angle between antenna and reflection point (in degrees) 
 
 %% 
-% Geocentric radii to receiving antenna and transmitting satellite
-Ra = Rs+Ha;
-% Rt = Rs+Ht; #UNUSED2
-
 % Functions to convert from position vector to complex number and vice versa:
-posgeo2complex = @(pos) complex(pos(2), pos(1))./Rs;
-complex2posgeo = @(cpx) [imag(cpx), real(cpx)].*Rs;  % in geo frame
+posgeo2complex = @(pos) complex(pos(1), pos(2))./Rs;
+complex2posgeo = @(cpx) [real(cpx), imag(cpx)].*Rs;  % in geo frame
 
 % Geocenter position in local frame:
 pos_cnt_loc = [0 -Rs];
@@ -34,11 +30,11 @@ pos_cnt_loc = [0 -Rs];
 % Receiving antenna position:
 pos_ant_loc = [0 Ha];  % in local frame
 pos_ant_geo = pos_ant_loc - pos_cnt_loc;  % in geocentric frame
-z1 = posgeo2complex(pos_ant_geo);  % as complex number
+z1 = posgeo2complex(pos_ant_geo);  % as complex number #CORRECT
 
 % Transmitting satellite position:
 [pos_trans_loc, pos_trans_geo] = get_satellite_position (e, Ha, Ht, Rs, 2);
-z2 = posgeo2complex(pos_trans_geo);
+z2 = posgeo2complex(pos_trans_geo); % #CORRECT
 
 % Polynomial coefficients:
 c4 = +conj(z1)*conj(z2);
@@ -62,31 +58,19 @@ u = us(ind);
 pos_spec_geo = complex2posgeo(u);  % in geocentric frame
 pos_spec_loc = pos_spec_geo + pos_cnt_loc;  % in local frame
 
-% pos_spec_geo = complex2posgeo(u);  % in geocentric frame
-% pos_spec_loc = pos_spec_geo + pos_cnt_loc;  % in local frame
-
 % Point coordinates output results:
-x_spec = pos_spec_loc(1);
-y_spec = pos_spec_loc(2);
+x_spec = pos_spec_loc(1); % #CORRECT
+y_spec = pos_spec_loc(2); % #CORRECT
 x_trans = pos_trans_loc(1);
 y_trans = pos_trans_loc(2);
-%pos_spec = struct('geo',pos_spec_geo, 'loc',pos_spec_loc);
-%pos_trans = struct('geo',pos_trans_geo, 'loc',pos_trans_loc);
-%%pos_spec = struct();
-%%pos_spec.X = pos_spec_geo(:,1);
-%%pos_spec.Y = pos_spec_geo(:,2);
-%%pos_spec.x = pos_spec_loc(:,1);
-%%pos_spec.y = pos_spec_loc(:,2);
-%%%pos_spec.geo = pos_spec_geo;
-%%%pos_spec.loc = pos_spec_loc;
 
 %% Geocentric angle between antenna and reflection point
-geo_ang_as = rad2deg(angle(u));
+geo_ang_as = 90-rad2deg(angle(u));
 
 %% Grazing Angle
 
 % graz_ang = atand(cotd(geo_ang)-(Rs./Ra)./sind(geo_ang)); #OLD 
-graz_ang = 90+rad2deg(angle(z1/u-1));
+graz_ang = 90-rad2deg(angle(z1/u-1));
 
 end
 
